8de9dfccb3549d76029dfe3dfa200585d3b27daf
Kıvanç Çakmak <kivanccakmak@Kvancs-MacBook-Air.local>
2026-03-12 14:08:57 +0300

    Fix formula: use on_arm/on_intel blocks for proper Homebrew DSL

--- Formula/yaver.rb
+++ Formula/yaver.rb
@@ -5,20 +5,22 @@ class Yaver < Formula
   license "Proprietary"
 
   on_macos do
-    if Hardware::CPU.arm?
+    on_arm do
       url "https://github.com/kivanccakmak/yaver-cli/releases/download/v#{version}/yaver-darwin-arm64"
       sha256 "2cb028e0602a0d55061677bd0b99dde12d5c609a798a742c69be6474d6524d04"
-    else
+    end
+    on_intel do
       url "https://github.com/kivanccakmak/yaver-cli/releases/download/v#{version}/yaver-darwin-amd64"
       sha256 "10e11e672093a84d70040e17b002a25931e9d26877d52fe22fa010a0188d261d"
     end
   end
 
   on_linux do
-    if Hardware::CPU.arm?
+    on_arm do
       url "https://github.com/kivanccakmak/yaver-cli/releases/download/v#{version}/yaver-linux-arm64"
       sha256 "0dc10ff61bd2058e81d9335dd8972353fa45dd3f5592738d4db0f6e8eaa318c7"
-    else
+    end
+    on_intel do
       url "https://github.com/kivanccakmak/yaver-cli/releases/download/v#{version}/yaver-linux-amd64"
       sha256 "9ebd49b11a8291c8d7eeb520e7ceb36786193adeae445543c0c7a1b66d11c31f"
     end
