3b733e78f767342740adea6cb024bec189f0b493
Kıvanç Çakmak <kivanccakmak@Kvancs-MacBook-Air.local>
2026-03-22 19:50:09 +0300

    Update yaver to v1.41.0

--- Formula/yaver.rb
+++ Formula/yaver.rb
@@ -1,26 +1,26 @@
 class Yaver < Formula
   desc "AI coding agent on your phone — P2P remote control for Claude Code, Codex, Aider, Ollama"
   homepage "https://yaver.io"
-  version "1.40.0"
+  version "1.41.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.40.0/yaver-darwin-arm64"
-      sha256 "28fc5c389f2fce883048f7d30e4ece5a97631c341f7a97029d34372cef6ed6a5"
+      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.41.0/yaver-darwin-arm64"
+      sha256 "2356975cb8b1ed279680e3cc73827125439083baaa184d341d49264a6477668f"
     else
-      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.40.0/yaver-darwin-amd64"
-      sha256 "9fe6aa10a7912625812b8cfa297151ca344098c81a94409166239f21c0f51687"
+      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.41.0/yaver-darwin-amd64"
+      sha256 "716128cfa16a5d4f42e3bc6f9fedccf0ff8091a0189ad4112e6c6b04c90ce4e9"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.40.0/yaver-linux-arm64"
-      sha256 "40054ce738377f26f481ad7cec6b22dabe5c1922e0be00a999399e78592e8f0c"
+      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.41.0/yaver-linux-arm64"
+      sha256 "c816741928d3f914b00d544e4065ccf9e566794701aeae530a373196ed09149f"
     else
-      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.40.0/yaver-linux-amd64"
-      sha256 "2b6432d4c6531b3952efddd6febe41712b1d29ac86b91377a85b780acef2d099"
+      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.41.0/yaver-linux-amd64"
+      sha256 "c467f09a927d1a7768164aa7712ced9d4a14306768370cb4f6c74b8edf0dce7c"
     end
   end
 
