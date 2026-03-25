a3f79cc1623b0b51918479cea03a360dc5039b5f
Kıvanç Çakmak <kivanccakmak@Kvancs-MacBook-Air.local>
2026-03-25 18:31:09 +0300

    v1.46.0: E2E dev server proxy

--- Formula/yaver.rb
+++ Formula/yaver.rb
@@ -7,20 +7,20 @@ class Yaver < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.46.0/yaver-darwin-arm64"
-      sha256 "22458cd13e3e3d21e9dd2f0c6d69aa64dcdb0408c3d8e18c7e9a32a06546a4c6"
+      sha256 "462c7b9114be76666dd792cfed30df805ca4a3e11543dd9980b339320ad2da2e"
     else
       url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.46.0/yaver-darwin-amd64"
-      sha256 "3fda511965f5bb093a5470c57353ca4935b97bf20ea1f6c64cbb24dfa871c509"
+      sha256 "462c7b9114be76666dd792cfed30df805ca4a3e11543dd9980b339320ad2da2e"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.46.0/yaver-linux-arm64"
-      sha256 "9b53a1691f82b30d1aee0b783c1b7203c01c0e8c715d3769577b650ee19ae9e2"
+      sha256 "462c7b9114be76666dd792cfed30df805ca4a3e11543dd9980b339320ad2da2e"
     else
       url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.46.0/yaver-linux-amd64"
-      sha256 "43e7cb3ed552bf5d873d73e7fe14d0c190ef67489d352c7abe3736ca9291cb36"
+      sha256 "462c7b9114be76666dd792cfed30df805ca4a3e11543dd9980b339320ad2da2e"
     end
   end
 
