b5c13c8c3e21da89ecd6a3a538898cb1a2ec9718
Kıvanç Çakmak <kivanccakmak@Kvancs-MacBook-Air.local>
2026-03-25 20:20:38 +0300

    Fix per-platform SHA256 hashes for v1.46.0

--- Formula/yaver.rb
+++ Formula/yaver.rb
@@ -7,7 +7,7 @@ class Yaver < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.46.0/yaver-darwin-arm64"
-      sha256 "462c7b9114be76666dd792cfed30df805ca4a3e11543dd9980b339320ad2da2e"
+      sha256 "1327aedd730707e0a6913c6bbe6938cdeb41b1e2341b57378f46143c3a74063e"
     else
       url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.46.0/yaver-darwin-amd64"
       sha256 "462c7b9114be76666dd792cfed30df805ca4a3e11543dd9980b339320ad2da2e"
@@ -17,10 +17,10 @@ class Yaver < Formula
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.46.0/yaver-linux-arm64"
-      sha256 "462c7b9114be76666dd792cfed30df805ca4a3e11543dd9980b339320ad2da2e"
+      sha256 "4f10c6b14b42c3e26f23aba7fbbdde30320c29be36372d660c67bb34ede557b8"
     else
       url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.46.0/yaver-linux-amd64"
-      sha256 "462c7b9114be76666dd792cfed30df805ca4a3e11543dd9980b339320ad2da2e"
+      sha256 "ff1683b06aaac7b2f53d09455be2fdc823cf92f9465f7c8d451ded3e7d237971"
     end
   end
 
