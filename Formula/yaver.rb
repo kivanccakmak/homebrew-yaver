1adb2d775ba223788ef093a36e4976e5e5b4f6d7
Kıvanç Çakmak <kivanccakmak@Kvancs-MacBook-Air.local>
2026-03-27 01:58:18 +0300

    Bump to v1.59.0

--- Formula/yaver.rb
+++ Formula/yaver.rb
@@ -1,24 +1,24 @@
 class Yaver < Formula
   desc "AI coding agent on your phone — P2P remote control for Claude Code, Codex, Aider, Ollama"
   homepage "https://yaver.io"
-  version "1.58.0"
+  version "1.59.0"
   license "MIT"
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.58.0/yaver-darwin-arm64.tar.gz"
-      sha256 "f1c11fb2de719bca3bbcf2f0e803e5b288cedbe53ca02a1356103b8afad7e4a9"
+      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.59.0/yaver-darwin-arm64.tar.gz"
+      sha256 "25cd3a2bc220ab05fe789ee6e981df3b63336657edd9f97ffe191275e3cd9750"
     else
-      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.58.0/yaver-darwin-amd64.tar.gz"
-      sha256 "355224e3d8c02ad320393e7ef19a232cbde0ec6f184b221940c9ab9483b08553"
+      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.59.0/yaver-darwin-amd64.tar.gz"
+      sha256 "d9b341df5c16a9344d80de3d70717c85770633992ed6cb8a1fc7ba62f8ed0859"
     end
   end
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.58.0/yaver-linux-arm64.tar.gz"
-      sha256 "b7850d8486b56788b153ed8c635dd92bdf2362dfabc49a55d28e0999987c1ee7"
+      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.59.0/yaver-linux-arm64.tar.gz"
+      sha256 "ab98182aaf7356739998495491816a0a4063e79229421bfc46c13d005a2f3327"
     else
-      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.58.0/yaver-linux-amd64.tar.gz"
-      sha256 "bd0484defa629bda2dc9eb2cdf8dc50c6c8b8477cffc96d6c6e898a2c6bf0774"
+      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.59.0/yaver-linux-amd64.tar.gz"
+      sha256 "abc79ddcfa3ab8d8f07bcd7a67d9fa35c57d221dfd3357da9a6c12d224225ab2"
     end
   end
   def install
