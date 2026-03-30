d6186ad9fc6ea5731634d2a4d27fd11786b4d053
Kıvanç Çakmak <kivanccakmak@Kvancs-MacBook-Air.local>
2026-03-31 00:23:47 +0300

    v1.66.0 — super host

--- Formula/yaver.rb
+++ Formula/yaver.rb
@@ -1,24 +1,24 @@
 class Yaver < Formula
   desc "AI coding agent on your phone — P2P remote control for Claude Code, Codex, Aider, Ollama"
   homepage "https://yaver.io"
-  version "1.65.0"
+  version "1.66.0"
   license "MIT"
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.65.0/yaver-darwin-arm64.tar.gz"
-      sha256 "f4e7c468513e85b6e9b44df8d5c6d68eb95406970ef5dd6e66a9d961b076b205"
+      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.66.0/yaver-darwin-arm64.tar.gz"
+      sha256 "d6c551e8e6fc1083cf4e2934a05598884f2b0ea4b634d9a0d1e2816d01c3e0b0"
     else
-      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.65.0/yaver-darwin-amd64.tar.gz"
-      sha256 "c352c908b83e03554efba13d5b1a35a6c874409e69a96acf8599853ee7ca1f24"
+      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.66.0/yaver-darwin-amd64.tar.gz"
+      sha256 "fab72dea04b29c8754bd45c248f46a88ddec11b342f147bb47eb016412db683c"
     end
   end
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.65.0/yaver-linux-arm64.tar.gz"
-      sha256 "633f10fb5248799cecb7305e542c7addd6b653f0f28fad84b2e2aeff228c5117"
+      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.66.0/yaver-linux-arm64.tar.gz"
+      sha256 "5a253e0e9b3ca747641e0b680d71a183cc34165e1311a84cb15b93065edf1953"
     else
-      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.65.0/yaver-linux-amd64.tar.gz"
-      sha256 "b199e5ace8260d1db3ac1a7c40c39ca68a642679ba14c1f2f644ff2d489e7d7e"
+      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.66.0/yaver-linux-amd64.tar.gz"
+      sha256 "a8bf7da355b69befc54c7d664f167fbc186d484629b2cfad8679b54c3c6d1d33"
     end
   end
   def install
