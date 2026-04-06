5e60459050f6de6d5f2dfbec521f3bf957775355
Kıvanç Çakmak <kivanccakmak@Kvancs-MacBook-Air.local>
2026-04-06 12:09:18 +0300

    Bump to v1.74.0 — Guest Config

--- Formula/yaver.rb
+++ Formula/yaver.rb
@@ -1,24 +1,24 @@
 class Yaver < Formula
   desc "AI coding agent on your phone — P2P remote control for Claude Code, Codex, Aider, Ollama"
   homepage "https://yaver.io"
-  version "1.73.0"
+  version "1.74.0"
   license "MIT"
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.73.0/yaver-darwin-arm64.tar.gz"
-      sha256 "54b11b28c0edb52da1f1076716b52b8d7c5c2f45c05fc50cd7aaccc2be26ea98"
+      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.74.0/yaver-darwin-arm64.tar.gz"
+      sha256 "b7afe2f2c9cf944a14efcc19c6ec336fef6534f81f6d0f8bd3a5b7344b05a253"
     else
-      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.73.0/yaver-darwin-amd64.tar.gz"
-      sha256 "37e36efec50f275792d64e6eb590dd1f0e4b6545001049e40faa1d0bd057ae5a"
+      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.74.0/yaver-darwin-amd64.tar.gz"
+      sha256 "060e996ffe6652b959ad7d3de28d2a330fae3c79d1b699c8a072e70cf9000789"
     end
   end
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.73.0/yaver-linux-arm64.tar.gz"
-      sha256 "d19f20bdeb4699f8b558513c43c01560eb8d3df605c1b4f571dbed262463f4d9"
+      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.74.0/yaver-linux-arm64.tar.gz"
+      sha256 "9ec3032c103df896bc2fd4188157037bf4e942cc8c22f66c5738054a8c5c6c76"
     else
-      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.73.0/yaver-linux-amd64.tar.gz"
-      sha256 "a55f6f1c8edd460c607940f63c9a4606709c094f6ed6ac43225f81300647cdb3"
+      url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.74.0/yaver-linux-amd64.tar.gz"
+      sha256 "58e957657899e2ca518811359f394a95d8d7c358e9f528bbda15ef29f1470647"
     end
   end
   def install
