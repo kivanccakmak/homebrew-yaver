4c6c535671427602005762ea5b64eac553eafd98
Kıvanç Çakmak <kivanccakmak@Kvancs-MacBook-Air.local>
2026-04-12 10:32:33 +0300

    v1.87.0: fix archive to contain 'yaver' binary name

--- Formula/yaver.rb
+++ Formula/yaver.rb
@@ -7,20 +7,20 @@ class Yaver < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.87.0/yaver-darwin-arm64.tar.gz"
-      sha256 "68ee485197bbe10063a1d440e5304f4e0e1fc849bc560d38ed7e6b45261959ef"
+      sha256 "ca3f9dce382e617bdfb135d5de4ad93e618bf28d24554d9eec8f8a2257f1bda5"
     else
       url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.87.0/yaver-darwin-amd64.tar.gz"
-      sha256 "aace718590afcf2a1ccd4dd923dd01b00a20f414afdccf4c4ddd42bef0161c6f"
+      sha256 "6d7eab0b6baba07e13d75c3d1644f910a8c849ace5cf65c8025a2905c43eaba4"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.87.0/yaver-linux-arm64.tar.gz"
-      sha256 "3d7fca771d41612fc76b9769cec84d95e8afe8f7a4511d9978a290c9253b61ab"
+      sha256 "b9bd0630ffacdf54bbebdecb8eaa2e0e199f2c869f916bdb335d103c532b3813"
     else
       url "https://github.com/kivanccakmak/yaver.io/releases/download/v1.87.0/yaver-linux-amd64.tar.gz"
-      sha256 "1455fa828ea6e9b233b449f3cf7bb31c2d73fd29713ef077cef800384940b7af"
+      sha256 "fdc0a36795f93454f4abcda61ee089bb05827af6ca5f0f6263e3e3c0165ce179"
     end
   end
 
